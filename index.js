86bf8d3d4b1d7828f344129064ca34be7c50f272
mikaspell <work@melkee.ru>
2015-08-29 01:06:00 +0300

    added new capabilities for customize tracker && reworked transmission format parameters

--- index.js
+++ index.js
@@ -6,25 +6,36 @@ module.exports = {
         ],
         html: {
             "body:end": function() {
-                var config = this.options.pluginsConfig.yametrika || {};
-                
+                var config = this.options.pluginsConfig.yametrika || {},
+                    settings = "";
+
                 if (!config.number) {
                     throw "Need to option 'number' for Yandex Metrika plugin";
                 }
 
-                //if (config.webvisor === true) {
-                //    webvisor = ', webvisor:true'
-                //} 
+                if(typeof config.settings === 'object' && config.settings !== null) {
 
-               // if (config.clickmap === true) {
-               //     clickmap = ', clickmap:true'
-               // } 
+                    for (option in config.settings) {
 
-               // if (config.trackLinks === true) {
-               //     trackLinks = ', trackLinks:true'
-               // } 
+                        if (typeof config.settings[option] !== 'string') {
 
-                return "<script type='text/javascript'>(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter"+config.number+" = new Ya.Metrika({id:"+config.number+", webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName('script')[0], s = d.createElement('script'), f = function () { n.parentNode.insertBefore(s, n); }; s.type = 'text/javascript'; s.async = true; s.src = (d.location.protocol == 'https:' ? 'https:' : 'http:') + '//mc.yandex.ru/metrika/watch.js'; if (w.opera == '[object Opera]') { d.addEventListener('DOMContentLoaded', f, false); } else { f(); } })(document, window, 'yandex_metrika_callbacks');</script><noscript><div><img src='//mc.yandex.ru/watch/"+config.number+"' style='position:absolute; left:-9999px;' alt=''/></div></noscript>";
+                            settings += ", " + option + ":" + config.settings[option];
+
+                        } else {
+
+                            settings += ", " + option + ":'" + config.settings[option] + "'";
+                        }                        
+                    }
+                }
+
+                return "<script type='text/javascript'>(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { "
+                + "w.yaCounter" + config.number + " = new Ya.Metrika({id:" + config.number + settings + "});"
+                + " } catch(e) { } }); var n = d.getElementsByTagName('script')"
+                + "[0], s = d.createElement('script'), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "
+                + "'text/javascript'; s.async = true; s.src = (d.location.protocol == 'https:' ? 'https:' : 'http:') + "
+                + "'//mc.yandex.ru/metrika/watch.js'; if (w.opera == '[object Opera]') { d.addEventListener('DOMContentLoaded'"
+                + ", f, false); } else { f(); } })(document, window, 'yandex_metrika_callbacks');</script><noscript><div><img "
+                + "src='//mc.yandex.ru/watch/" + config.number + "' style='position:absolute; left:-9999px;' alt=''/></div></noscript>";
             }
         }
     }
